f520c1be80dcab2547a5acb15656b0bc587ed415
aramirezm <aramirez.unimarc@gmail.com>
2025-07-04 19:49:23 -0400

    feat: add StarRating component and integrate it into ProductCard for displaying product ratings

--- src/components/TopMenu.tsx
+++ src/components/TopMenu.tsx
@@ -4,14 +4,14 @@ export default function TopMenu() {
   return (
     <div className="sticky z-10 top-0 h-16 border-b bg-white lg:py-2.5">
       <div className="px-6 flex items-center justify-between space-x-4">
-        <h5 hidden className="text-2xl text-gray-600 font-medium lg:block">
+        <h5  className="text-2xl text-gray-600 font-medium lg:block">
           Dashboard
         </h5>
         <button className="w-12 h-16 -mr-2 border-r lg:hidden">
           <CiMenuBurger size={30} />
         </button>
         <div className="flex space-x-2">
-          <div hidden className="md:block">
+          <div  className="md:block">
             <div className="relative flex items-center text-gray-400 focus-within:text-cyan-400">
               <span className="absolute left-4 h-6 flex items-center pr-3 border-r border-gray-300">
                 <CiSearch />
